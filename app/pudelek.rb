0c11bce96b2569b0202a61de01333c9cd08eb553
Tomasz Janowski <tomasz.janowski@rea-group.com>
2013-03-18 19:07:53 +1100

    Fixed rfc 3339 times

--- app/pudelek.rb
+++ app/pudelek.rb
@@ -4,8 +4,7 @@ require 'nokogiri'
 module PudelekRSSFeed
   class Pudelek < Sinatra::Base
 
-    XML_TIMESTAMP = '%Y-%m-%dT%H:%M:%S%z'
-    RSS_ENTRIES = 20
+    RSS_ENTRIES = 120
 
     get '/' do
       content_type 'application/atom+xml', :charset => 'utf-8'
@@ -21,6 +20,13 @@ Cache Size = #{Utils::PageCache.count}
 
     private
 
+    def to_rfc3339 time
+      base = time.strftime('%Y-%m-%dT%H:%M:%S')
+      tz = time.strftime('%z')
+      tz_rfc = "#{tz[0..2]}:#{tz[3..4]}"
+      "#{base}#{tz_rfc}"
+    end
+
     def articles
       PudelekRSSFeed::PudelekMainPage.new.all.sort.reverse[0..(RSS_ENTRIES-1)]
     end
@@ -28,16 +34,16 @@ Cache Size = #{Utils::PageCache.count}
     def rss
       builder = Nokogiri::XML::Builder.new do |xml|
         xml.feed(:xmlns => "http://www.w3.org/2005/Atom") {
-          xml.link(:href => "http://janaz.pl/", :rel => 'self')
+          xml.link(:href => "http://www.pudelek.pl/", :rel => 'self')
           xml.title 'Pudelek by Janaz'
-          xml.id "http://janaz.pl/"
-          xml.updated Time.now.strftime(XML_TIMESTAMP)
+          xml.id "http://www.pudelek.pl/"
+          xml.updated to_rfc3339(Time.now)
           articles.each do |e|
             xml.entry {
               xml.title  e.title
               xml.link(:type => "text/html", :rel => "alternate", :href => e.url)
               xml.id e.article_id
-              xml.updated e.time.strftime(XML_TIMESTAMP)
+              xml.updated to_rfc3339(e.time)
               xml.content e.content, :type => 'html'
               xml.author {
                 xml.name 'pudelek.pl'
